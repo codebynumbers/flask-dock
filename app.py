66928169efc2df9276c808a609fc0d3eed7f613d
Rob Harrigan <harrigan.rob@gmail.com>
2017-03-01 23:11:53 -0500

    Again

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized - FEATURE C!!!'
+    return 'Flask Dockerized - FEATURE C, again!!!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
